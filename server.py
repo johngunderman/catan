4bb64bcee07e75a2e3073c0a93d97e2c71fefe08
Josh Snyder <josh@code406.com>
2011-11-09 23:38:47 -0500

    Proof of concept with Tornado long polling.
    We have blocking/unblocking, and pass-through to Flask.

--- server.py
+++ server.py
@@ -1,8 +1,43 @@
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
+from tornado.web import RequestHandler, Application, asynchronous, FallbackHandler
 from catan import app
+from time import sleep
 
-http_server = HTTPServer(WSGIContainer(app))
+waiters = set()
+
+class BlockHandler(RequestHandler):
+    @asynchronous
+    def get(self):
+	waiters.add(self.callback)
+        print("Exiting from async.")
+
+    def callback(self, result):
+        # Closed client connection
+        if self.request.connection.stream.closed():
+            return
+        self.finish(result)
+
+    def on_connection_close(self):
+        waiters.remove(self.callback)
+
+class ReleaseHandler(RequestHandler):
+    def get(self):
+        self.write("{0} waiting requests released".format(len(waiters)))
+        for callback in waiters:
+            callback(self.request.remote_ip)
+
+wsgi_app = WSGIContainer(app) 
+application = Application([
+    (r"/block", BlockHandler),
+    (r"/unblock", ReleaseHandler),
+    (r".*", FallbackHandler, dict(fallback=wsgi_app))
+])
+
+http_server = HTTPServer(application)
 http_server.listen(5000)
 IOLoop.instance().start()
+
+
+
