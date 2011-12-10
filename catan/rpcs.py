c7911aea366f059bac0d6bd6f0fce66bc6a8076f
Josh Snyder <josh@code406.com>
2011-12-10 05:18:05 -0500

    Begin real work on start_game() RPC
    - Refactor some initialization code into models
    - Make tweaks to models in order to support RPC code
    - Make the RPC return JSON (ish, see more)
    
    The RPC can be run by doing:
    >>> from catan import rpcs
    >>> rpcs.start_game(0,0)
    
    It tries to return JSON, but due to some issues, it fails to return
    syntactically correct JSON. I'm working on it.

--- catan/rpcs.py
+++ catan/rpcs.py
@@ -1,41 +1,17 @@
-import random
+from database import db_session
+from models import *
+import json
 
-DESERT_CHIT = 7
-DESERT_POSITION = 8
-CHITS = [5, 10, 8, 2, 9, 3, 4, 6, 11, 6, 11, 3, 4, 5, 12, 8, 10, 9]
-
-class Terrain:
-    FOREST = 1
-    PASTURE = 2
-    GRAIN = 3
-    BRICK = 4
-    ORE = 5
-    DESERT = 6
-
-"""Test ideas:
-create_board should be len(19)
-it should 2-ples
-
-"""
-def start_game():
-    Game
-    def create_board():
-       hexes = [Terrain.FOREST]*4 + [Terrain.PASTURE]*4 + [Terrain.GRAIN]*4 + [Terrain.BRICK]*3 + [Terrain.ORE]*3
-       random.shuffle(hexes) #Shuffle the hexes
-       hexes = zip(hexes, CHITS) #Give them chits
-       hexes = hexes[:DESERT_POSITION] + [(Terrain.DESERT, DESERT_CHIT)] + hexes[DESERT_POSITION:] #Place the desert in a fixed place
-       return hexes
-
-    return create_board()
-
-def fake_create_game()
-    for u in User.query.limit(4):
-        
-
-def create_game():
-    g = Game()
+def start_game(id, sequence):
+    g = Game(); #This will eventually be done earlier, like in create_game()
+    g.start();
     
+    sequence = Log.query.count() #This needs to be not what it is right now.  Make MySQL do it?
+    g.log.append(
+        Log(sequence, json.dumps({ "action" : "hexes_placed", "args": [i.json() for i in g.hexes]}))
+    )
 
-def join_game():
-    
+    db_session.add(g);
+    db_session.commit();
 
+    return {"result": "success", "log": [i.Action for i in Log.query.filter(Log.Sequence >= sequence).all()]}
