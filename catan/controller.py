6e9471e971eb4e33a9140a3647cfe927e4b8c90c
Rafael <rafael@case.edu>
2011-12-13 00:01:41 -0500

    fixed up some python syntax, added hasResources function and incorportated it into current actions in controller
    Signed-off-by: Rafael Lopez <rafael@case.edu>

--- catan/controller.py
+++ catan/controller.py
@@ -3,7 +3,7 @@ from models import *
 from string import replace
 
 import vertices
-import json
+import json      
 
 def get_game(gameid):
     return Game.query.get(gameid)
@@ -30,60 +30,75 @@ def start_game(userid, game):
     return "success"
 
 def build_settlement(userid, game, vertex):
-    p = decompress(vertex)
-    if isvalid(p): #TODO: we also need to figure out whether the game logic allows it
-        s = Settlement(vertex, userid, Settlement.TOWN)
-        game.settlements.append(s)
-        game.log({ "action" : "settlement_built", "args" : [userid, vertex]})
+    p = vertices.decompress(vertex)
+    if vertices.isvalid(p): #TODO: we also need to figure out whether the game logic allows it
+        player = GamePlayer.query.filter(GamePlayer.GameID == game.GameID).filter(GamePlayer.UserID == userid).first()
+        if (player.hasResources(models.BuildTypes.SETTLEMENT)):
+            s = Settlement(vertex, userid, Settlement.TOWN)
+            game.settlements.append(s)
+            game.log({ "action" : "settlement_built", "args" : [userid, vertex]})
 
-        db_session.commit()
+            db_session.commit()
 
-        return "success"
+            add_log("success", id, sequence)
+        else:
+            return add_log("failure", id, sequence)
     else:
-        return "failure"
+        return add_log("failure", id, sequence)
 		
 def upgrade_settlement(gameid, userid, vertex, sequence):
-    p = decompress(vertex)
-    if isvalid(p): # game logic for allowing an upgrade is different from initially placing a settlement though
-        g = Game.query.get(id)
+    p = vertices.decompress(vertex)
+    if vertices.isvalid(p): # game logic for allowing an upgrade is different from initially placing a settlement though
+        player = GamePlayer.query.filter(GamePlayer.GameID == game.GameID).filter(GamePlayer.UserID == userid).first()
+        if(player.hasResources(models.BuildTypes.CITY)):
+            g = Game.query.get(id)
 
-        s = Settlement(vertex, userid, Settlement.CITY)
-        g.settlements.append(s)
-        g.log({ "action" : "settlement_upgraded", "args" : [userid, vertex]})
+            s = Settlement(vertex, userid, Settlement.CITY)
+            g.settlements.append(s)
+            g.log({ "action" : "settlement_upgraded", "args" : [userid, vertex]})
 
-        db_session.commit()
+            db_session.commit()
 
-        return add_log("success", id, sequence)
+            return add_log("success", id, sequence)
+        else:
+            return add_log("failure", id, sequence)
     else:
         return add_log("failure", id, sequence)
 
 def build_road(game, userid, vertex1, vertex2, sequence):
-    p1 = decompress(vertex1)
-    p2 = decompress(vertex2)
-    if isvalid(p1) and isvalid(p2): #game logic for allowing a road is different from allowing a settlement though
-        r = Road(vertex1, vertex2, userid)
-        g.roads.append(r)
-        g.log({ "action" : "road_built", "args" : [userid, vertex1, vertex2]})
+    p1 = vertices.decompress(vertex1)
+    p2 = vertices.decompress(vertex2)
+    if vertices.isvalid(p1) and vertices.isvalid(p2): #game logic for allowing a road is different from allowing a settlement though
+        player = GamePlayer.query.filter(GamePlayer.GameID == game.GameID).filter(GamePlayer.UserID == userid).first()
+        if(player.hasResources(models.BuildTypes.ROAD)):
+            r = Road(vertex1, vertex2, userid)
+            g.roads.append(r)
+            g.log({ "action" : "road_built", "args" : [userid, vertex1, vertex2]})
 
-        db_session.commit() # TODO: check if we now have longest road. 
-                            # Also, I think you can place a road and cut across someone elses road ?
+            db_session.commit() # TODO: check if we now have longest road. 
+                                # Also, I think you can place a road and cut across someone elses road ?
 
-        return add_log("success", id, sequence)
+            return add_log("success", id, sequence)
+        else:
+            return add_log("failure", id, sequence)
     else:
         return add_log("failure", id, sequence)
 
 def development_card(game, userid):
     #check if user has 1 sheep 1 ore 1 wheat
-    if hasResources(userid, DEVCARD):
+    player = GamePlayer.query.filter(GamePlayer.GameID == game.GameID).filter(GamePlayer.UserID == userid).first()
+    if player.hasResources(DEVCARD):
         g = Game.query.get(id)
-
-        player = GamePlayer.query.filter(GamePlayer.GameID == game.GameID).filter(GamePlayer.UserID == userid).first()
-
+        
         card = GameCards.draw()
         player.getCard(card)
 		
         g.log({ "action" : "devcard_bought", "args" : [userid]}) #public
         #g.log({ "action" : "devcard_bought", "args" : [userid, card]}) #private
+        
+        return add_log("success", id, sequence)
+    else:
+        return add_log("failure", id, sequence)
 
 """
 def move_robber():
