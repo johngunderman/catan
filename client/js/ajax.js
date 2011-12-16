9c8b30f3c3e60f8eac95a3dc1036dd8e3a46b8c3
John Gunderman <johngunderman@gmail.com>
2011-12-16 04:15:13 -0500

    refactor event handling, some events still need to be filled out

--- client/js/ajax.js
+++ client/js/ajax.js
@@ -1,3 +1,44 @@
+// All our handlers for action events
+// req events are handled seperately, as we only care about them if they
+// are on the top of the log. See handleResponseJson().
+handlers = {
+    "resources_gained"    : handle_resources_gained,
+    "hexes_placed"        : handle_hexes_placed,
+    "settlement_built"    : handle_settlement_built,
+    "settlement_upgraded" : handle_settlement_upgraded
+}
+
+req_handlers = {
+    "req_turn"            : handle_req_turn,
+    "req_setup"           : handle_req_setup
+}
+
+function handle_resources_gained(log_entry) {
+
+}
+
+function handle_req_setup(log_entry) {
+
+}
+
+function handle_hexes_placed(log_entry) {
+    initBoard(log_entry.args);
+}
+
+function handle_settlement_built(log_entry) {
+
+}
+
+function handle_settlement_upgraded(log_entry) {
+
+}
+
+function handle_req_turn(log_entry) {
+
+}
+
+
+
 // The result of the ajax request will json which is then passed to
 // the given callback func.
 function makeAjaxRequest(url, params, callbackFunc) {
@@ -7,10 +48,6 @@ function makeAjaxRequest(url, params, callbackFunc) {
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 
-            var myJson = JSON.parse(xmlhttp.responseText);
-            if (myJson.sequence) {
-                sequenceNum = myJson.sequence;
-            }
             console.log("Server Response: " + xmlhttp.responseText);
 
             callbackFunc(xmlhttp.responseText);
@@ -23,23 +60,43 @@ function makeAjaxRequest(url, params, callbackFunc) {
 }
 
 
-// currently a huge hack, just so we can get the starting board layout.
-function startGameRequest() {
+function handleResponseJson(json) {
+    var myJson = JSON.parse(json);
+
+    if(myJson.response && myJson.log
+       && myJson.sequence && myJson.response == "success") {
+
+        sequenceNum = myJson.sequence;
 
 
-    var start_game_callback = function(json) {
-        var myJson = JSON.parse(json);
 
-        if (myJson.log[0].action != "hexes_placed") {
-            console.log("ERROR: data returned from /start_game is unexpected");
+        // take care of everything else
+        var log = myJson.log;
+
+        for(var x = 0; x < myJson.log.length; x++) {
+            if (handlers[log[x].action]) {
+                handlers[log[x].action](log[x]);
+            }
         }
-        else {
-            var hexes = myJson.log[0].args;
 
-            initBoard(hexes);
+        var top = myJson.log[myJson.log.length - 1];
+
+        // handle req_handlers if need be
+        if (req_handlers[top.action] && top.user == userID) {
+            req_handlers[top.action](top);
         }
+
 
     }
+    else {
+        // stuff is really messed up, so go ahead and reload the page
+        window.location.reload();
+    }
+}
+
+
+// currently a huge hack, just so we can get the starting board layout.
+function startGameRequest() {
 
     var create_game_callback = function(json) {
         gameID = parseInt(json);
@@ -49,10 +106,9 @@ function startGameRequest() {
                         "?user=" + userID
                         + "&game=" + gameID
                         + "&sequence=" + sequenceNum,
-                        start_game_callback);
+                        handleResponseJson);
     }
 
     makeAjaxRequest(HOSTNAME + "/create_game", "?user=" + userID,
                    create_game_callback);
 }
-
